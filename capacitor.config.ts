e0cad6801cff143781a509634e0c953da7a114d3
Dtqlqwq <2172118834@qq.com>
2026-04-08 20:47:07 +0800

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,20 +1,20 @@
 import { CapacitorConfig } from '@capacitor/cli';
-
 const config: CapacitorConfig = {
   appId: 'com.pacex.app',
   appName: 'Pulse - 跑步追踪',
   webDir: 'dist',
   server: {
-    androidScheme: 'https'
+    androidScheme: 'https',
+    allowNavigation: [
+      '*.supabase.co',
+      '*.siliconflow.cn',
+      'pacex.top'
+    ]
   },
   plugins: {
     Geolocation: {
       enableHighAccuracy: true
-    },
-    PushNotifications: {
-      presentationOptions: ["badge", "sound", "alert"]
     }
   }
 };
-
 export default config;
